ab8b650c8f0b970c4c17ac07a1bc9163fc315b41
PathlothSanthosh <151642193+PathlothSanthosh@users.noreply.github.com>
2023-11-21 22:33:00 +0530

    index.js

--- index.js
+++ index.js
@@ -1,44 +1,60 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration Form</title>
-    <style>
-        /* Your styles here */
-    </style>
-</head>
-<body>
+// Function to validate the date of birth
+function validateDateOfBirth() {
+    var dobInput = document.getElementById("dob");
+    var dobValue = new Date(dobInput.value);
+    var currentDate = new Date();
+    var minDate = new Date(currentDate.getFullYear() - 55, currentDate.getMonth(), currentDate.getDate());
+    var maxDate = new Date(currentDate.getFullYear() - 18, currentDate.getMonth(), currentDate.getDate());
 
-    <form id="registrationForm" action="#" method="post">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required>
+    if (dobValue < minDate || dobValue > maxDate) {
+        alert("Date of birth must be between 18 and 55 years.");
+        dobInput.value = ""; // Clear the invalid date
+    } else {
+        alert("Thank you for providing a valid date of birth.");
+    }
+}
 
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required>
+// Function to load saved data from web storage
+function loadSavedData() {
+    var savedData = JSON.parse(localStorage.getItem("registrationData")) || [];
+    var tableBody = document.getElementById("tableBody");
 
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required>
+    // Clear table body
+    tableBody.innerHTML = "";
 
-        <button type="button" onclick="validateForm()">Register</button>
-    </form>
-
-    <script>
-        function validateForm() {
-            var username = document.getElementById('username').value;
-            var email = document.getElementById('email').value;
-            var password = document.getElementById('password').value;
-
-            // Simple validation: check if fields are not empty
-            if (username === "" || email === "" || password === "") {
-                alert("Please fill in all fields");
-            } else {
-                // If validation passes, you can submit the form or perform other actions
-                alert("Registration successful!");
-                document.getElementById('registrationForm').submit(); // Uncomment to submit the form
-            }
+    // Populate table with saved data
+    savedData.forEach(function (data) {
+        var row = tableBody.insertRow();
+        for (var key in data) {
+            var cell = row.insertCell();
+            cell.textContent = data[key];
         }
-    </script>
+    });
+}
 
-</body>
-</html>
+// Function to handle form submission
+document.getElementById("registrationForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var dob = document.getElementById("dob").value;
+    var acceptTerms = document.getElementById("acceptTerms").checked;
+
+    var formData = { name: name, email: email, password: password, dob: dob, acceptTerms: acceptTerms };
+
+    // Save data to web storage
+    var savedData = JSON.parse(localStorage.getItem("registrationData")) || [];
+    savedData.push(formData);
+    localStorage.setItem("registrationData", JSON.stringify(savedData));
+
+    // Load and display saved data
+    loadSavedData();
+    
+    // Validate date of birth and show an alert
+    validateDateOfBirth();
+});
+
+// Load saved data when the page loads
+window.onload = loadSavedData;
